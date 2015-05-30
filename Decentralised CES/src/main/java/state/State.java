54c632ceecc19682460b00ad7bcc557fbdc994ad
Yuchen Wang <yuchen_wng@hotmail.com>
2015-05-30 23:51:39 +0100

    Creating the state

---
+++ Decentralised CES/src/main/java/state/State.java
@@ -0,0 +1,19 @@
+package state;
+import state.StateType;
+public class State {
+
+    public final StateType state;
+
+    public State()
+    {
+        this.state = StateType.ChildrenRequest;
+    }
+
+    //Constructor
+
+    //IncrementState
+
+    //GetState
+
+
+}
